3180fb5db18bfabd021b78a2183dc67f62e15fdf
Shiroi | Crypto <93323888+ShiroiCrypto@users.noreply.github.com>
2025-05-02 19:42:52 -0300

    Create saldo.js

---
+++ ComandosSlash/Economia/saldo.js
@@ -0,0 +1,13 @@
+const Discord = require("discord.js");
+const corSombrio = parseInt('2d1433', 16); // Roxo sombrio
+const corVermelhoEscuro = parseInt('8b0000', 16); // Vermelho escuro
+
+module.exports = {
+  name: "saldo", // Nome do comando
+  description: "Comando para ver o seu saldo, ou de outra pessoa.", // Descrição do comando
+  type: Discord.ApplicationCommandType.ChatInput,
+
+  run: async (client, interaction) => {
+
+  }
+};
